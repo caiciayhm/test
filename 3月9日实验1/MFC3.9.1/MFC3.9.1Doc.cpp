ccb0198bf7411cdff02ce8814b945fef8b71a7d0
caicaiyhm <2716827427@qq.com>
2020-04-30 22:58:08 +0800

    2020.4.30

---
+++ 3月9日实验1/MFC3.9.1/MFC3.9.1Doc.cpp
@@ -0,0 +1,138 @@
+﻿
+// MFC3.9.1Doc.cpp: CMFC391Doc 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFC3.9.1.h"
+#endif
+
+#include "MFC3.9.1Doc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CMFC391Doc
+
+IMPLEMENT_DYNCREATE(CMFC391Doc, CDocument)
+
+BEGIN_MESSAGE_MAP(CMFC391Doc, CDocument)
+END_MESSAGE_MAP()
+
+
+// CMFC391Doc 构造/析构
+
+CMFC391Doc::CMFC391Doc() noexcept
+{
+	// TODO: 在此添加一次性构造代码
+
+}
+
+CMFC391Doc::~CMFC391Doc()
+{
+}
+
+BOOL CMFC391Doc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: 在此添加重新初始化代码
+	// (SDI 文档将重用该文档)
+
+	return TRUE;
+}
+
+
+
+
+// CMFC391Doc 序列化
+
+void CMFC391Doc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: 在此添加存储代码
+	}
+	else
+	{
+		// TODO: 在此添加加载代码
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// 缩略图的支持
+void CMFC391Doc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// 修改此代码以绘制文档数据
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// 搜索处理程序的支持
+void CMFC391Doc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// 从文档数据设置搜索内容。
+	// 内容部分应由“;”分隔
+
+	// 例如:     strSearchContent = _T("point;rectangle;circle;ole object;")；
+	SetSearchContent(strSearchContent);
+}
+
+void CMFC391Doc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CMFC391Doc 诊断
+
+#ifdef _DEBUG
+void CMFC391Doc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CMFC391Doc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CMFC391Doc 命令
---
+++ 3月9日实验1/MFC3.9.1/MFC3.9.1View.cpp
@@ -0,0 +1,95 @@
+﻿
+// MFC3.9.1View.cpp: CMFC391View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFC3.9.1.h"
+#endif
+
+#include "MFC3.9.1Doc.h"
+#include "MFC3.9.1View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFC391View
+
+IMPLEMENT_DYNCREATE(CMFC391View, CView)
+
+BEGIN_MESSAGE_MAP(CMFC391View, CView)
+	ON_WM_LBUTTONDOWN()
+END_MESSAGE_MAP()
+
+// CMFC391View 构造/析构
+
+CMFC391View::CMFC391View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMFC391View::~CMFC391View()
+{
+}
+
+BOOL CMFC391View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFC391View 绘图
+
+void CMFC391View::OnDraw(CDC* pDC)
+{
+	CMFC391Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+	CRect cr;
+	this->GetClientRect(cr);
+	pDC->Ellipse(cr);
+}
+
+
+// CMFC391View 诊断
+
+#ifdef _DEBUG
+void CMFC391View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFC391View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFC391Doc* CMFC391View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFC391Doc)));
+	return (CMFC391Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFC391View 消息处理程序
+
+
+void CMFC391View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	int r = rand() % (40 - 5) + 5;
+	CRect cr(point.x - r, point.y - r, point.x + r, point.y + r);
+	CView::OnLButtonDown(nFlags, point);
+}
