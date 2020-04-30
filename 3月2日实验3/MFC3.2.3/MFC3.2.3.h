3d2f7dcf43f7e9a73c092510a9b26dff6c99581c
caicaiyhm <2716827427@qq.com>
2020-04-30 22:54:01 +0800

    2020.4.30

---
+++ 3月2日实验3/MFC3.2.3/MFC3.2.3.h
@@ -0,0 +1,33 @@
+﻿
+// MFC3.2.3.h: MFC3.2.3 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CMFC323App:
+// 有关此类的实现，请参阅 MFC3.2.3.cpp
+//
+
+class CMFC323App : public CWinApp
+{
+public:
+	CMFC323App() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFC323App theApp;
---
+++ 3月2日实验3/MFC3.2.3/MFC3.2.3View.cpp
@@ -0,0 +1,94 @@
+﻿
+// MFC3.2.3View.cpp: CMFC323View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFC3.2.3.h"
+#endif
+
+#include "MFC3.2.3Doc.h"
+#include "MFC3.2.3View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFC323View
+
+IMPLEMENT_DYNCREATE(CMFC323View, CView)
+
+BEGIN_MESSAGE_MAP(CMFC323View, CView)
+	ON_WM_LBUTTONUP()
+END_MESSAGE_MAP()
+
+// CMFC323View 构造/析构
+
+CMFC323View::CMFC323View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMFC323View::~CMFC323View()
+{
+}
+
+BOOL CMFC323View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFC323View 绘图
+
+void CMFC323View::OnDraw(CDC* /*pDC*/)
+{
+	CMFC323Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CMFC323View 诊断
+
+#ifdef _DEBUG
+void CMFC323View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFC323View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFC323Doc* CMFC323View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFC323Doc)));
+	return (CMFC323Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFC323View 消息处理程序
+
+
+void CMFC323View::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CRect cr;
+	this->GetClientRect(&cr);
+	CClientDC dc(this);
+	dc.Ellipse(cr);
+	CView::OnLButtonUp(nFlags, point);
+}
