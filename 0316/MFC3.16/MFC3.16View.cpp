70adc432472c5bae03ca60d5e8894a8db3398e57
caicaiyhm <2716827427@qq.com>
2020-05-02 23:39:49 +0800

    2020.05.02

---
+++ 0316/MFC3.16/MFC3.16View.cpp
@@ -0,0 +1,129 @@
+﻿
+// MFC3.16View.cpp: CMFC316View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFC3.16.h"
+#endif
+
+#include "MFC3.16Doc.h"
+#include "MFC3.16View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFC316View
+
+IMPLEMENT_DYNCREATE(CMFC316View, CView)
+
+BEGIN_MESSAGE_MAP(CMFC316View, CView)
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+	ON_WM_MOUSEMOVE()
+END_MESSAGE_MAP()
+
+// CMFC316View 构造/析构
+
+CMFC316View::CMFC316View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMFC316View::~CMFC316View()
+{
+}
+
+BOOL CMFC316View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFC316View 绘图
+
+void CMFC316View::OnDraw(CDC* pDC)
+{
+	CMFC316Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CMFC316View 诊断
+
+#ifdef _DEBUG
+void CMFC316View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFC316View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFC316Doc* CMFC316View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFC316Doc)));
+	return (CMFC316Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFC316View 消息处理程序
+
+
+void CMFC316View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	judge = true;
+	begin = point;
+	now = point;
+	CView::OnLButtonDown(nFlags, point);
+}
+
+
+void CMFC316View::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	if (judge)
+	{
+		judge = false;
+	}
+
+	CView::OnLButtonUp(nFlags, point);
+}
+
+
+void CMFC316View::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	if (judge)
+	{
+		CClientDC dc(this);
+		CString s;
+		s.Format(_T("%d,%d"), point.x, point.y);
+		dc.TextOutW(40, 30, s);
+		CBrush b = dc.GetBkColor();
+		CBrush *newbrush = dc.SelectObject(&b);
+		int oldMove = dc.SetROP2(R2_NOTXORPEN);
+		dc.Rectangle(&CRect(begin, now));
+		dc.Rectangle(&CRect(begin, point));
+		now = point;
+		dc.SelectObject(newbrush);
+		dc.SetROP2(oldMove);
+	}
+	CView::OnMouseMove(nFlags, point);
+}
---
+++ 0316/MFC3.16/MFC3.16View.h
@@ -0,0 +1,54 @@
+﻿
+// MFC3.16View.h: CMFC316View 类的接口
+//
+
+#pragma once
+
+class CMFC316View : public CView
+{
+protected: // 仅从序列化创建
+	CMFC316View() noexcept;
+	DECLARE_DYNCREATE(CMFC316View)
+
+// 特性
+public:
+	CMFC316Doc* GetDocument() const;
+
+// 操作
+public:
+
+	bool judge = false;
+	CPoint begin;
+	CPoint now;
+
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~CMFC316View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+};
+
+#ifndef _DEBUG  // MFC3.16View.cpp 中的调试版本
+inline CMFC316Doc* CMFC316View::GetDocument() const
+   { return reinterpret_cast<CMFC316Doc*>(m_pDocument); }
+#endif
+
