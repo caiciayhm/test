749c52e87945d083b9896c6a269709ebaa919c76
caicaiyhm <2716827427@qq.com>
2020-05-06 23:14:43 +0800

    2020.05.06

---
+++ 0323_2/0323_2/0323_2View.cpp
@@ -0,0 +1,116 @@
+﻿
+// 0323_2View.cpp: CMy03232View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "0323_2.h"
+#endif
+
+#include "0323_2Doc.h"
+#include "0323_2View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMy03232View
+
+IMPLEMENT_DYNCREATE(CMy03232View, CView)
+
+BEGIN_MESSAGE_MAP(CMy03232View, CView)
+	ON_COMMAND(ID_SHOWYUAN, &CMy03232View::OnShowyuan)
+	ON_WM_TIMER()
+END_MESSAGE_MAP()
+
+// CMy03232View 构造/析构
+
+CMy03232View::CMy03232View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMy03232View::~CMy03232View()
+{
+}
+
+BOOL CMy03232View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMy03232View 绘图
+
+void CMy03232View::OnDraw(CDC* pDC)
+{
+	CMy03232Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+	CRect cr;
+	this->GetClientRect(&cr);
+	point.x = (cr.right - cr.left) / 2;
+	point.y = (cr.bottom - cr.top) / 2;
+	CRect rect1(point.x - r, point.y - r, point.x + r, point.y + r);
+	rect = rect1;
+
+	CPen pen;
+	a = rand() % 255;
+	b = rand() % 255;
+	c = rand() % 255;
+	pen.CreatePen(PS_SOLID, 5, RGB(a, b, c));
+	CPen *oldpen = (pDC->SelectObject(&pen));
+	pDC->Ellipse(rect);
+	pen.DeleteObject();
+}
+
+
+// CMy03232View 诊断
+
+#ifdef _DEBUG
+void CMy03232View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMy03232View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMy03232Doc* CMy03232View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMy03232Doc)));
+	return (CMy03232Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMy03232View 消息处理程序
+
+
+void CMy03232View::OnShowyuan()
+{
+	// TODO: 在此添加命令处理程序代码
+	SetTimer(0, 100, NULL);
+}
+
+
+void CMy03232View::OnTimer(UINT_PTR nIDEvent)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	r = t * 5;
+	t++;
+	Invalidate();
+	CView::OnTimer(nIDEvent);
+}
---
+++ 0323_2/0323_2/0323_2View.h
@@ -0,0 +1,53 @@
+﻿
+// 0323_2View.h: CMy03232View 类的接口
+//
+
+#pragma once
+
+
+class CMy03232View : public CView
+{
+protected: // 仅从序列化创建
+	CMy03232View() noexcept;
+	DECLARE_DYNCREATE(CMy03232View)
+
+// 特性
+public:
+	CMy03232Doc* GetDocument() const;
+
+// 操作
+public:
+	CPoint point;
+	CRect rect;
+	int r = 0;
+	int t = 1;
+	int a, b, c;
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~CMy03232View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnShowyuan();
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+};
+
+#ifndef _DEBUG  // 0323_2View.cpp 中的调试版本
+inline CMy03232Doc* CMy03232View::GetDocument() const
+   { return reinterpret_cast<CMy03232Doc*>(m_pDocument); }
+#endif
+
