b3e57266814b49cf6f4bed1958ec6536c9b4dbe0
caicaiyhm <2716827427@qq.com>
2020-05-06 19:26:45 +0800

    2020.05.066

---
+++ 0330/0330/0330.h
@@ -0,0 +1,33 @@
+﻿
+// 0330.h: 0330 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CMy0330App:
+// 有关此类的实现，请参阅 0330.cpp
+//
+
+class CMy0330App : public CWinApp
+{
+public:
+	CMy0330App() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMy0330App theApp;
---
+++ 0330/0330/0330View.cpp
@@ -0,0 +1,127 @@
+﻿
+// 0330View.cpp: CMy0330View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "0330.h"
+#endif
+
+#include "0330Doc.h"
+#include "0330View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMy0330View
+
+IMPLEMENT_DYNCREATE(CMy0330View, CView)
+
+BEGIN_MESSAGE_MAP(CMy0330View, CView)
+	ON_COMMAND(ID_FILE_OPEN, &CMy0330View::OnFileOpen)
+END_MESSAGE_MAP()
+
+// CMy0330View 构造/析构
+
+CMy0330View::CMy0330View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMy0330View::~CMy0330View()
+{
+}
+
+BOOL CMy0330View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMy0330View 绘图
+
+void CMy0330View::OnDraw(CDC* /*pDC*/)
+{
+	CMy0330Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CMy0330View 诊断
+
+#ifdef _DEBUG
+void CMy0330View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMy0330View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMy0330Doc* CMy0330View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMy0330Doc)));
+	return (CMy0330Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMy0330View 消息处理程序
+
+
+void CMy0330View::OnFileOpen()
+{
+	// TODO: 在此添加命令处理程序代码
+	CFileDialog cfd(true);
+	int r = cfd.DoModal();
+	CClientDC dc(this);
+	if (r == IDOK)
+	{
+		CString filename=cfd.GetPathName();
+		
+		dc.TextOutW(200, 300, filename);
+		CImage img;
+		img.Load(filename);
+		int w, h, sx, sy;
+		{
+			
+			CRect rect;
+			GetClientRect(&rect);
+
+			float rect_ratio = 1.0*rect.Width() / rect.Height();
+			float img_ratio = 1.0*img.GetWidth() / img.GetHeight();
+
+			if (rect_ratio > img_ratio)
+			{
+				h = rect.Height();
+				w = img_ratio * h;
+				sx = (rect.Width() - w) / 2;
+				sy = 0;
+			}
+			else
+			{
+				w = rect.Width();
+				h = w / img_ratio;
+				sx = 0;
+				sy = (rect.Height() - h) / 2;
+			}
+		}
+		
+		img.Draw(dc.m_hDC, sx, sy, w ,h );
+
+	}
+}
