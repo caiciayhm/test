7f7611d71a180665f705a71d91c48f516d6f752e
caicaiyhm <2716827427@qq.com>
2020-05-09 16:08:05 +0800

    2020.05.09

---
+++ 0413_1/0413(1)/0413(1).h
@@ -0,0 +1,33 @@
+﻿
+// 0413(1).h: 0413(1) 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CMy04131App:
+// 有关此类的实现，请参阅 0413(1).cpp
+//
+
+class CMy04131App : public CWinApp
+{
+public:
+	CMy04131App() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMy04131App theApp;
---
+++ 0413_1/0413(1)/0413(1)View.h
@@ -0,0 +1,57 @@
+﻿
+// 0413(1)View.h: CMy04131View 类的接口
+//
+
+#pragma once
+
+
+class CMy04131View : public CView
+{
+protected: // 仅从序列化创建
+	CMy04131View() noexcept;
+	DECLARE_DYNCREATE(CMy04131View)
+
+// 特性
+public:
+	CMy04131Doc* GetDocument() const;
+
+// 操作
+public:
+
+	bool judge;
+	CPoint m_point;
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CMy04131View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+//	afx_msg void OnPaint();
+};
+
+#ifndef _DEBUG  // 0413(1)View.cpp 中的调试版本
+inline CMy04131Doc* CMy04131View::GetDocument() const
+   { return reinterpret_cast<CMy04131Doc*>(m_pDocument); }
+#endif
+
