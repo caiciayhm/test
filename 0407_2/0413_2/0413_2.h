fbb562e8708c11e22364a38a9ae95eafb646f1f1
caicaiyhm <2716827427@qq.com>
2020-05-18 17:15:56 +0800

    2020.05.18,改之前的0413_2(写错文件名）

---
+++ 0407_2/0413_2/0413_2.h
@@ -0,0 +1,33 @@
+﻿
+// 0413_2.h: 0413_2 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CMy04132App:
+// 有关此类的实现，请参阅 0413_2.cpp
+//
+
+class CMy04132App : public CWinApp
+{
+public:
+	CMy04132App() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMy04132App theApp;
---
+++ 0407_2/0413_2/0413_2View.h
@@ -0,0 +1,48 @@
+﻿
+// 0413_2View.h: CMy04132View 类的接口
+//
+
+#pragma once
+
+
+class CMy04132View : public CView
+{
+protected: // 仅从序列化创建
+	CMy04132View() noexcept;
+	DECLARE_DYNCREATE(CMy04132View)
+
+// 特性
+public:
+	CMy04132Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~CMy04132View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnShowdlog();
+};
+
+#ifndef _DEBUG  // 0413_2View.cpp 中的调试版本
+inline CMy04132Doc* CMy04132View::GetDocument() const
+   { return reinterpret_cast<CMy04132Doc*>(m_pDocument); }
+#endif
+
